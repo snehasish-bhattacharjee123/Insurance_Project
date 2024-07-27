dd020e838ce3ef1ed51b2ba440f88341cfb2b153
developerWebAmplifier <developer@web-amplifier.com>
2024-07-27 23:21:10 +0530

    changes

--- resources/views/frontend/exc/extend.blade.php
+++ resources/views/frontend/exc/extend.blade.php
@@ -4,3 +4,4 @@
 @yield('content')
 
 @include('frontend.inc.footer')
+@yield('script')
--- resources/views/frontend/post.blade.php
+++ resources/views/frontend/post.blade.php
@@ -8,6 +8,26 @@
                 <h2 class="profile-name">Suman Roy</h2>
             </div>
             <div class="profile-caption">
+                <p class="short-text" data-full-text="Lorem ipsum dolor sit amet  suman sumansumansumansumansumansumansumansumansumansumansumansuman">
+                </p>
+                <a href="javascript:void(0);" class="more-link" onclick="toggleText(this)">See more</a>
+            </div>
+            <div class="card-image">
+                <img src="iphone.webp" alt="" style="object-fit: cover;">
+            </div>
+            <div class="description">
+                <span class="long-text" data-full-text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum doloremque facere distinctio, voluptates numquam sunt eligendi dignissimos cum repudiandae explicabo, excepturi doloribus eaque veritatis animi laudantium illum vero iusto qui?"></span>
+                <a href="javascript:void(0);" class="more-link-description" onclick="toggleDescriptionText(this)" style="margin-left: 10px; margin-bootom: 20px;">See more</a>
+            </div>
+        </div>
+    </div>
+    <div class="card-container">
+        <div class="card">
+            <div class="card-profile-image">
+                <img src="suman.jpg" alt="">
+                <h2 class="profile-name">Suman Roy</h2>
+            </div>
+            <div class="profile-caption">
                 <p class="short-text" data-full-text="Lorem ipsum dolor sit amet, consectetur adipisicing elit...">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit...
                 </p>
@@ -17,105 +37,80 @@
                 <img src="iphone.webp" alt="" style="object-fit: cover;">
             </div>
             <div class="description">
-                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum doloremque facere distinctio, voluptates numquam sunt eligendi dignissimos cum repudiandae explicabo, excepturi doloribus eaque veritatis animi laudantium illum vero iusto qui?</span>
-            </div>
-        </div>
-    </div>
-    <div class="card-container">
-        <div class="card">
-            <div class="card-profile-image">
-                <img src="suman.jpg" alt="">
-                <h2 class="profile-name">Suman Roy</h2>
-            </div>
-            <div class="profile-caption">
-                <p class="short-text" data-full-text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto molestias laboriosam consequuntur voluptatum maxime modi quas sapiente, nobis aut tempore qui est vitae neque velit numquam esse quo quis beatae.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto molestias laboriosam consequuntur voluptatum maxime modi quas sapiente, nobis aut tempore qui est vitae neque velit numquam esse quo quis beatae.">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto molestias laboriosam...
-                </p>
-                <a href="javascript:void(0);" class="more-link" onclick="toggleText(this)">See more</a>
-            </div>
-            <div class="card-image">
-                <img src="suman.jpg" alt="" style="object-fit: cover;">
-            </div>
-            <div class="description">
-                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum doloremque facere distinctio, voluptates numquam sunt eligendi dignissimos cum repudiandae explicabo, excepturi doloribus eaque veritatis animi laudantium illum vero iusto qui?</span>
-            </div>
-        </div>
-    </div>
-    <div class="card-container">
-        <div class="card">
-            <div class="card-profile-image">
-                <img src="suman.jpg" alt="">
-                <h2 class="profile-name">Suman Roy</h2>
-            </div>
-            <div class="profile-caption">
-                <p class="short-text" data-full-text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto molestias laboriosam consequuntur voluptatum maxime modi quas sapiente, nobis aut tempore qui est vitae neque velit numquam esse quo quis beatae.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto molestias laboriosam consequuntur voluptatum maxime modi quas sapiente, nobis aut tempore qui est vitae neque velit numquam esse quo quis beatae.">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto molestias laboriosam...
-                </p>
-                <a href="javascript:void(0);" class="more-link" onclick="toggleText(this)">See more</a>
-            </div>
-            <div class="card-image">
-                <img src="suman.jpg" alt="" style="object-fit: cover;">
-            </div>
-            <div class="description">
-                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum doloremque facere distinctio, voluptates numquam sunt eligendi dignissimos cum repudiandae explicabo, excepturi doloribus eaque veritatis animi laudantium illum vero iusto qui?</span>
-            </div>
-        </div>
-    </div>
-    <div class="card-container">
-        <div class="card">
-            <div class="card-profile-image">
-                <img src="suman.jpg" alt="">
-                <h2 class="profile-name">Suman Roy</h2>
-            </div>
-            <div class="profile-caption">
-                <p class="short-text" data-full-text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto molestias laboriosam consequuntur voluptatum maxime modi quas sapiente, nobis aut tempore qui est vitae neque velit numquam esse quo quis beatae.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto molestias laboriosam consequuntur voluptatum maxime modi quas sapiente, nobis aut tempore qui est vitae neque velit numquam esse quo quis beatae.">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto molestias laboriosam...
-                </p>
-                <a href="javascript:void(0);" class="more-link" onclick="toggleText(this)">See more</a>
-            </div>
-            <div class="card-image">
-                <img src="suman.jpg" alt="" style="object-fit: cover;">
-            </div>
-            <div class="description">
-                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum doloremque facere distinctio, voluptates numquam sunt eligendi dignissimos cum repudiandae explicabo, excepturi doloribus eaque veritatis animi laudantium illum vero iusto qui?</span>
+                <span class="long-text" data-full-text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum doloremque facere distinctio, voluptates numquam sunt eligendi dignissimos cum repudiandae explicabo, excepturi doloribus eaque veritatis animi laudantium illum vero iusto qui?"></span>
+                <a href="javascript:void(0);" class="more-link-description" onclick="toggleDescriptionText(this)" style="margin-left: 10px; margin-bootom: 20px;">See more</a>
             </div>
         </div>
     </div>
 
     <script>
         function toggleText(link) {
-    const paragraph = link.previousElementSibling;
-    const fullText = paragraph.getAttribute('data-full-text');
-    const isExpanded = paragraph.classList.toggle('expanded');
+            const paragraph = link.previousElementSibling;
+            const fullText = paragraph.getAttribute('data-full-text');
+            const isExpanded = paragraph.classList.toggle('expanded');
 
-    if (isExpanded) {
-        paragraph.textContent = fullText;
-        link.textContent = 'See less';
-    } else {
-        const truncatedText = fullText.substring(0, 200) + '...';
-        paragraph.textContent = truncatedText;
-        link.textContent = 'See more';
-    }
+            if (isExpanded) {
+                paragraph.textContent = fullText;
+                link.textContent = 'See less';
+            } else {
+                const truncatedText = fullText.substring(0, 40) + '...';
+                paragraph.textContent = truncatedText;
+                link.textContent = 'See more';
+            }
 
-    // Adjust the card's height based on expanded content
-    const card = link.closest('.card');
-    card.style.height = 'auto'; // Reset height to auto to accommodate expanded content
-    const newHeight = card.scrollHeight; // Get the new height based on content
-    card.style.height = newHeight + 'px'; // Set the card's height to the new height
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    const paragraphs = document.querySelectorAll('.profile-caption .short-text');
-    paragraphs.forEach(paragraph => {
-        const fullText = paragraph.getAttribute('data-full-text');
-        if (fullText.length > 200) {
-            paragraph.textContent = fullText.substring(0, 200) + '...';
+            // Adjust the card's height based on expanded content
+            const card = link.closest('.card');
+            card.style.height = 'auto'; // Reset height to auto to accommodate expanded content
+            const newHeight = card.scrollHeight; // Get the new height based on content
+            card.style.height = newHeight + 'px'; // Set the card's height to the new height
         }
-    });
-});
+
+        function toggleDescriptionText(link) {
+            const span = link.previousElementSibling;
+            const fullText = span.getAttribute('data-full-text');
+            const isExpanded = span.classList.toggle('expanded');
+
+            if (isExpanded) {
+                span.textContent = fullText;
+                link.textContent = 'See less';
+            } else {
+                const truncatedText = fullText.substring(0, 200) + '...';
+                span.textContent = truncatedText;
+                link.textContent = 'See more';
+            }
+
+            // Adjust the card's height based on expanded content
+            const card = link.closest('.card');
+            card.style.height = 'auto'; // Reset height to auto to accommodate expanded content
+            const newHeight = card.scrollHeight; // Get the new height based on content
+            card.style.height = newHeight + 'px'; // Set the card's height to the new height
+        }
+
+        document.addEventListener('DOMContentLoaded', function() {
+            const paragraphs = document.querySelectorAll('.profile-caption .short-text');
+            paragraphs.forEach(paragraph => {
+                const fullText = paragraph.getAttribute('data-full-text');
+                if (fullText.length > 40) {
+                    paragraph.textContent = fullText.substring(0, 40) + '...';
+                } else {
+                    paragraph.textContent = fullText; // Show normal text
+                    const moreLink = paragraph.nextElementSibling;
+                    moreLink.style.display = 'none'; // Hide "See more" link
+                }
+            });
+
+            const spans = document.querySelectorAll('.description .long-text');
+            spans.forEach(span => {
+                const fullText = span.getAttribute('data-full-text');
+                if (fullText.length > 200) {
+                    span.textContent = fullText.substring(0, 200) + '...';
+                } else {
+                    span.textContent = fullText; // Show normal text
+                    const moreLink = span.nextElementSibling;
+                    moreLink.style.display = 'none'; // Hide "See more" link
+                }
+            });
+        });
     </script>
     
 @endsection
