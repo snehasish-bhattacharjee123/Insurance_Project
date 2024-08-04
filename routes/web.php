c9f87698bcc5fe2bb168017fddb17191e80b7db8
developerWebAmplifier <115920062+SumanR07@users.noreply.github.com>
2024-08-04 23:37:23 +0530

    c

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */  
-//  suman changes
-// snehaish there
-
+//  1st changes 
 Route::get('/',[\App\Http\Controllers\Frontend\FrontendController::class, 'index'])->name('first.page');
 Route::get('/about',[\App\Http\Controllers\Frontend\AboutController::class, 'index'])->name('about.page');
 Route::get('/service',[\App\Http\Controllers\Frontend\ServiceController::class, 'index'])->name('service.page');
